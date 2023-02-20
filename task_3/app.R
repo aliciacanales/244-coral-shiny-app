3bcf90fcb42d7186d8a2f096e29c0db6006178d4
aliciacanales <canalesalicia06@gmail.com>
2023-02-20 14:24:45 -0800

    Worked on tabs

--- task_3/app.R
+++ task_3/app.R
@@ -11,8 +11,9 @@ library(tidyverse)
 library(shiny)
 library(bslib)
 library(lubridate)
+library(here)
 
-coral <- readxl::read_excel('coral_data_244.xls') %>% 
+coral <- readxl::read_excel(here('data', 'coral_data_244.xls')) %>% 
   mutate(date = ymd(date))
 
 my_theme <- bs_theme(
@@ -25,40 +26,46 @@ my_theme <- bs_theme(
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = my_theme,
-
-    # Application title
-    titlePanel("Spatial Dsitribution of Coral across Northshore Moorea"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+                navbarPage("title",
+                           tabPanel('About the Study',
+                                    mainPanel(
+                                      h1('Summary of the study'),
+                                      h2('To visualize the spatial distribution of coral sizes across the northshore of Moorea based on genus and available settlement area'),
+                                      h6('Alicia Canales, Kat Mackay, Danielle Hoekstra')
+                                    )),
+                           tabPanel('Map 1',
+                                   sidebarLayout(
+                                     sidebarPanel("Genus",
+                                                  checkboxGroupInput(inputId = 'pick_species',
+                                                                     label = 'Choose species',
+                                                                     choices = unique(coral$genus)
+                                                                     )
+                                                  ),
+                                     mainPanel("Output",
+                                               plotOutput('coral_plot')
+                                               )
+                                   ) 
+                                   ),
+                           tabPanel('Date'),
+                            tabPanel('map 2')
+                )
 )
+    # Application title
+  
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  coral_reactive <- reactive({
+    coral %>%
+      filter(genus %in% input$pick_species)
+  })
+  
+  output$coral_plot <- renderPlot(
+    ggplot(data = coral_reactive(), aes(x = length, y = width)) +
+      geom_point(aes(color = genus))
+  )
+  
 }
 
 # Run the application 
